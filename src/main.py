258400ecb753af5e2cb83495a6827e4938fed193
davnajork <david@najork.org>
2020-12-11 16:00:11 -0800

    begin stats integration

--- src/main.py
+++ src/main.py
@@ -20,25 +20,31 @@ def main():
     if not days:
         log(logfile, "No new vessel movement reports.")
         return
-    dates = [day.strftime("%Y_%m_%d") for day in days]
-    date_strs = [day.strftime("%B %d %Y") for day in days]
+    dates = (day.strftime("%Y_%m_%d") for day in days)
+    date_strs = (day.strftime("%B %d %Y") for day in days)
     sync_required = False
     for date, date_str, day in zip(dates, date_strs, days):
-        data_frames = []
+        # data_frames = []
         if not os.path.exists("../cache/" + date):
             if not sync_required:
                 sync_required = True
             input_filename = day.strftime("%Y-%m-%d.csv")
-            data_frames.append(import_report("../temp/" + input_filename))
+            maritime_report = import_report("../temp/" + input_filename)
+            # data_frames.append(import_report("../temp/" + input_filename))
             log(logfile, "Importing data from " + input_filename + "...")
             os.makedirs(os.path.dirname("../cache/" + date + "/"), exist_ok=True)
-            map_data = [[], []] # [charleston, savannah]
-            for df in data_frames:
-                for i, map in enumerate(map_data, start=0):
-                    map.append(df[i])
-            id = [date + "/ch", date + "/sv"]
-            for i in range(len(map_data)):
-                create_csv_cache(map_data[i], id[i])
+            # map_data = [[], []] # [charleston, savannah]
+            # stats_data = [[], []] # [charleston, savannah]
+            # for df in data_frames:
+            #     for j in range(len(map_data)):
+            #         map_data[j].append(df[j][0])
+            maritime_data = [[port[0] for port in maritime_report],
+                             [port[1] for port in maritime_report]]
+            caches = ((date + "/ch-geo", date + "/sv-geo"),
+                      (date + "/ch", date + "/sv"))
+            for i in range(len(caches)):
+                for j in range(len(maritime_data[i])):
+                    create_csv_cache([maritime_data[i][j]], caches[i][j])
             log(logfile, "Created cache for " + date_str + ".")
         else:
             # latest data already exists in cache
@@ -52,32 +58,59 @@ def main():
                       if f.is_dir()], reverse=True)
         for subdir in dirs:
             path = "../cache/" + subdir + "/*.csv"
-            csv = []
+            # [[geo_ch, geo_sv], [stats_ch, stats_sv]]
+            # csvs = [[None, None],[None, None]]
+
+            # geo_ch_cache = None
+            # geo_sv_cache = None
+            # stats_ch_cache = None
+            # stats_sv_cache = None
+
+            # [geo_ch, geo_sv, stats_ch, stats_sv]
+            caches = [None, None, None, None]
             for filename in glob.glob(path):
-                if filename.endswith(".csv") and ("ch.csv" in filename or "sv.csv" in filename):
-                    csv.append(filename)
-            if not csv:
-                log(logfile, "Empty cache found: " + subdir)
+                if filename.endswith(".csv"):
+                    if "ch-geo.csv" in filename:
+                        caches[0] = pd.read_csv(filename)
+                    elif "sv-geo.csv" in filename:
+                        caches[1] = pd.read_csv(filename)
+                    elif "ch.csv" in filename:
+                        caches[2] = pd.read_csv(filename)
+                    elif "sv.csv" in filename:
+                        caches[3] = pd.read_csv(filename)
+            if True in [isinstance(cache, type(None)) for cache in caches]:
+                log(logfile, "Empty/partial cache found: " + subdir)
                 continue
-            ch_cache = pd.read_csv(csv[1])
-            sv_cache = pd.read_csv(csv[0])
-            data_frames.append([ch_cache, sv_cache,
-                                pd.concat([ch_cache, sv_cache])])
+            data_frames.append([caches[0], caches[1],
+                                pd.concat([caches[0], caches[1]]),
+                                caches[2], caches[3],
+                                pd.concat([caches[2], caches[3]])])
+            # data_frames.append([ch_cache, sv_cache,
+            #                     pd.concat([ch_cache, sv_cache])])
 
         map_data = [[], [], []] # [charleston, savannah, aggregate]
+        stats_data = [[], [], []]
         span = 0
         temp_ch = []
         temp_sv = []
+        temp_ch_stats = []
+        temp_sv_stats = []
         for df in data_frames:
             if span < 7:
                 map_data[0].append(df[0])
                 map_data[1].append(df[1])
                 map_data[2].append(df[2])
+                stats_data[0].append(df[3])
+                stats_data[1].append(df[4])
+                stats_data[2].append(df[5])
                 span += 1
             else:
                 temp_ch.append(df[0])
                 temp_sv.append(df[1])
                 map_data[2].append(df[2])
+                temp_ch_stats.append(df[3])
+                temp_ch_stats.append(df[4])
+                stats_data[2].append(df[5])
         log(logfile, "Loaded the last " + str(span) + " days for level two plots.")
         create_xlsx_cache(map_data[0] + temp_ch, "master-ch")
         create_xlsx_cache(map_data[1] + temp_sv, "master-sv")
@@ -85,17 +118,17 @@ def main():
         create_csv_cache(map_data[1] + temp_sv, "master-sv")
         for i in range(len(map_data)):
             map_data[i] = pd.concat(map_data[i]).reset_index().drop("index", axis=1)
-        geoplots = {"lvl2_CH":None, "lvl2_SV":None, "lvl1":None}
+        geo_plots = {"lvl2_CH":None, "lvl2_SV":None, "lvl1":None}
         zooms = (10.5, 11.5, 8)
         sizes = ((431, 707), (431, 707), (431, 707))
         heat = (False, False, False)
         token = open("../conf/.mapbox_token").read()
-        for i, level in enumerate(geoplots.keys()):
-            geoplots[level] = generate_geoplots(map_data[i], zooms[i], sizes[i], heat[i], token)
-        # output geoplots in an interactive HTML format
-        pio.write_html(geoplots["lvl1"], file="../html/level_one.html", auto_open=False)
-        pio.write_html(geoplots["lvl2_CH"], file="../html/level_two_charleston.html", auto_open=False)
-        pio.write_html(geoplots["lvl2_SV"], file="../html/level_two_savannah.html", auto_open=False)
+        for i, level in enumerate(geo_plots.keys()):
+            geo_plots[level] = generate_geo_plot(map_data[i], zooms[i], sizes[i], heat[i], token)
+        # output geo_plots in an interactive HTML format
+        pio.write_html(geo_plots["lvl1"], file="../html/level_one.html", auto_open=False)
+        pio.write_html(geo_plots["lvl2_CH"], file="../html/level_two_charleston.html", auto_open=False)
+        pio.write_html(geo_plots["lvl2_SV"], file="../html/level_two_savannah.html", auto_open=False)
         log(logfile, "Finished program execution successfully.")
         log(logfile, "Preparing to upload...")
     else:
--- src/plot.py
+++ src/plot.py
@@ -1,27 +1,27 @@
 # NOTES:
 # https://plotly.com/python/mapbox-layers/ (example used)
 
-from import_vessel_data import *
+# from import_maritime_data import *
 
 import plotly.express as px
 import pandas as pd
 
-def generate_geoplots(map_data, zoom, size, heatmap_enabled, token):
+def generate_geo_plot(map_data, zoom, size, heatmap_enabled, token):
     fig = None
     if not heatmap_enabled:
         fig = px.scatter_mapbox(map_data, hover_name="Name",
                                 lat="Latitude", lon="Longitude",
-                                hover_data=["Date/Time UTC", "COURSE",
-                                "Max speed kn", "Mean speed kn", "LOA ft"],
+                                hover_data=["Date/Time UTC", "Course Behavior",
+                                "Max Speed kn", "Mean Speed kn", "WSPD mph", "LOA ft"],
                                 color_discrete_sequence=["white"],
                                 zoom=zoom, height=size[0], width=size[1])
         fig.update_traces(marker_size=4)
     else:
         fig = px.density_mapbox(map_data, hover_name="Name",
-                                z="Max speed kn", radius=5,
+                                z="Max Speed kn", radius=5,
                                 lat="Latitude", lon="Longitude",
-                                hover_data=["Date/Time UTC", "COURSE",
-                                "Max speed kn", "Mean speed kn", "LOA ft"],
+                                hover_data=["Date/Time UTC", "Course Behavior",
+                                "Max Speed kn", "Mean Speed kn", "WSPD mph", "LOA ft"],
                                 zoom=zoom, height=size[0], width=size[1])
     fig.update_layout(
         mapbox_accesstoken=token,
@@ -29,4 +29,24 @@ def generate_geoplots(map_data, zoom, size, heatmap_enabled, token):
         showlegend=False
     )
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
+def generate_vspd_hist(map_data):
+    fig = None
+    return fig
+
+def generate_wspd_hist(map_data):
+    fig = None
+    return fig
+
+def generate_density_plot(map_data):
+    fig = None
+    return fig
+
+def generate_strip_plot(map_data):
+    fig = None
+    return fig
+
+def generate_line_plot(map_data):
+    fig = None
     return fig
