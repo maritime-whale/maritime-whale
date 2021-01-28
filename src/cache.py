85ce31b470591754f8434c459ceeab6ec168e2bb
davnajork <david@najork.org>
2021-01-28 15:39:08 -0800

    remove shebang lines from files that don't contain a main (https://google.github.io/styleguide/pyguide.html#37-shebang-line)

--- src/cache.py
+++ src/cache.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # Copyright 2020 The Maritime Whale Authors. All rights reserved.
 # Use of this source code is governed by an MIT-style license that can be
 # found in the LICENSE.txt file.
