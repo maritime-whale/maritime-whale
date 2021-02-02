9c1d638cfb2df320eacc7cd08f57f28351b9a1c6
davnajork <david@najork.org>
2021-02-01 19:55:35 -0800

    remove TODO meetpas

--- src/meetpass.py
+++ src/meetpass.py
@@ -14,8 +14,6 @@ import scipy
 MEET_PASS_TIME_TOL = 1 # in hours
 MIN_DISTANCE = 0.1 # in degrees
 
-# TODO: add inline comments to meetpass
-
 def _calc_dist(lat1, long1, lat2, long2):
     """Computes the distance between two geolocations"""
     return ((lat1 - lat2)**2 + (long1 - long2)**2)**0.5
